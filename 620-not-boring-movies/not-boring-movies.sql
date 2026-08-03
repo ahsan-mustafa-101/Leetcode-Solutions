41a25a677958afc3640020372fb8a78da3b113b1
Ahsan Mustafa <ahsandev101@gmail.com>
2026-08-03 22:58:40 +0500

    Time: 192 ms (50.99%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,2 +1,2 @@
 -- Write your PostgreSQL query statement below
-select id, movie, description, rating from Cinema where description <> 'boring' and id % 2 = 1 order by rating desc;
+select * from Cinema where description <> 'boring' and id % 2 = 1 order by rating desc;
